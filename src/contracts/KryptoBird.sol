3f5dc699053254140f4184d6a393526090aeb3c5
Tom Maenhout <tom.maenhout@gmail.com>
2022-01-04 21:27:48 -0300

    first_migration

---
+++ src/contracts/KryptoBird.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract KryptoBird {
+    string private name;
+    string private symbol;
+
+    constructor () {
+        name = 'KryptoBird';
+        symbol = 'KBIRDZ';
+    }
+
+}
