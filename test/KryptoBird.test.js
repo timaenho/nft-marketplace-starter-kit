ac7a824f53a3e1493e7d657628c5f2e926d00291
timaenho <tom.maenhout@gmail.com>
2022-02-01 21:27:15 -0300

    javascript

---
+++ test/KryptoBird.test.js
@@ -0,0 +1,28 @@
+const {assert} = require('chai')
+
+
+const KryptoBird = artifacts.require('./KryptoBird')
+
+// check for chai
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract ('KryptoBird', (accounts) => {
+    let contract
+
+    // testing container - describe
+    describe('deployment', async () => {
+        //test samples with writing it
+        it('deploys succesfully', async()=>{
+            contract  = await KryptoBird.deployed()
+            const address = contract.address
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, 0x0)
+        })
+
+    })
+})
