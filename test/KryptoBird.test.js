acb27cc4316e426c0a4aa86218686b32236976cd
timaenho <tom.maenhout@gmail.com>
2022-02-04 20:09:38 -0300

    test

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -11,18 +11,24 @@ require('chai')
 
 contract ('KryptoBird', (accounts) => {
     let contract
+    //before tells us to run this before anything else
+    before( async () => {
+            contract = await KryptoBird.deployed()
+    }
+    )
 
     // testing container - describe
     describe('deployment', async () => {
         //test samples with writing it
         it('deploys succesfully', async()=>{
-            contract  = await KryptoBird.deployed()
             const address = contract.address
             assert.notEqual(address, '')
             assert.notEqual(address, null)
             assert.notEqual(address, undefined)
             assert.notEqual(address, 0x0)
         })
+        //test that the name matches on our contract using assert.equal function
+        //test that the symbol matches with the assert.equal
         it('name contract matches', async()=>{
             const name = await contract.name()
             assert.equal(name, 'KryptoBird')
@@ -34,7 +40,40 @@ contract ('KryptoBird', (accounts) => {
 
     })
 
-    //test that the name matches on our contract using assert.equal function
-    //test that the symbol matches with the assert.equal
-    //
+    describe ('minting', async () => {
+        it ('creates a new token', async () =>{
+            const result = await contract.mint('https...1')
+            const totalSupply = await contract.totalSupply()
+
+            //success
+            assert.equal(totalSupply, 1)
+            const event = result.logs[0].args
+            assert.equal(event._from, '0x0000000000000000000000000000000000000000', 'from is the contract')
+            assert.equal(event._to,accounts[0],'to is msg.sender')
+
+            //failure
+            await contract.mint('https...1').should.be.rejected
+        })
+    })
+
+    describe('indexing', async () => {
+            it('lists the KryptoBirdz', async() => {
+                await contract.mint('https...2')
+                await contract.mint('https...3')
+                await contract.mint('https...4')
+                const totalSupply = await contract.totalSupply()
+
+                let result = []
+                let KryptoBird
+                for(i = 0; i < result.length; i++){
+                 KryptoBird = await contract.kryptoBirdz[i]
+                 result.push(KryptoBird)
+                }
+            })
+    })
+
+
+
+
+   
 })
