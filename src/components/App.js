dfdcd130a1a830f17ff80c527b7a101a88866604
timaenho <tom.maenhout@gmail.com>
2022-02-12 21:12:24 -0300

    front

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider"
 import '@metamask/legacy-web3'
 import KryptoBird from '../abis/KryptoBird.json'
-
+import {MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn} from 'mdb-react-ui-kit'
+import './App.css'
 
 class App extends Component {
     // loads in the beginning
@@ -40,7 +41,7 @@ class App extends Component {
     async loadBlockchainData(){
         const web3 = window.web3
         const accounts = await web3.eth.getAccounts()
-        this.setState({account: accounts})
+        this.setState({account: accounts[0]})
 
         //create a constant js variable networkId which is set to 
         // to the blockchain network id
@@ -67,7 +68,7 @@ class App extends Component {
                     kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird]
                 })   
             }
-                    console.log(this.state.kryptoBirdz) 
+                
         // error handling         
         }else{
             window.alert('Smart contract not deployed')
@@ -76,9 +77,24 @@ class App extends Component {
 
     // with minting we are sending information and we need to specify the account 
 
+    mint = (kryptoBird) => {
+        this.state.contract.methods.mint(kryptoBird).send({from:this.state.account})
+        .once("receipt", (receipt) =>{
+            this.setState({
+                kryptoBirdz: [...this.state.kryptoBirdz, kryptoBird]
+            })
+        })
+    }
+
+    //complete the minting form
+    // 1. create a text input with a place holder 
+    // 'add file location '
+    // 2. Create another input button with the type submit 
+
     render () {
         return (
             <div>
+                {console.log(this.state.kryptoBirdz)}
                 <nav className= 'navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
                     <div className='navbar-brand col-sm-3 col-md-3 mr-3'style={{color:'white'}} >
                         Krypto Birdz NFTs (Non Fungible Tokens)
@@ -91,6 +107,57 @@ class App extends Component {
                     </li>
                 </ul>
                 </nav>
+                <div className='container-fluid mt-1'>
+                    <div className = 'row'>
+                        <main role = 'main'
+                        className="col-lg-12 d-flex test-center">
+                            <div className='content mr-auto ml-auto'
+                             style={{opacity:'0.8'}}>
+                                <h1 style={{color:"white"}}>KryptoBirdz- NFT Marketplace</h1>    
+                                <form onSubmit={(event)=>{
+                                    event.preventDefault()
+                                    const kryptoBird = this.kryptoBird.value
+                                    this.mint(kryptoBird) 
+                                }}> 
+                                    <input
+                                    type = 'text'
+                                    placeholder="Add a file location"
+                                    className=" mb-1"
+                                    ref= {(input) => {
+                                        this.kryptoBird = input
+                                    }}
+                                    />
+                                    <input
+                                    style = {{margin:'6px'}}
+                                    type = 'submit'
+                                    className="btn btn-primary btn-black"
+                                    value = 'MINT'
+                                    />
+                                </form>
+                            </div>
+                        </main>
+                    </div>
+                    <hr/>
+                    <div className = 'row textCenter'>
+                         {this.state.kryptoBirdz.map((kryptoBird, key)=>{
+                             return(
+                            <div>
+                                <div>
+                                    <MDBCard className="token img" style={{maxWidth:'22rem'}}>
+                                    <MDBCardImage src={kryptoBird} position='top' height={'250rem'} style={{marginRight:'4px'}}/>
+                                    <MDBCardBody>
+                                        <MDBCardTitle>KryptoBirdz</MDBCardTitle>
+                                        <MDBCardText>The KryptoBirdz are 20 uniquely generated</MDBCardText>
+                                        <MDBBtn href={kryptoBird}>Download</MDBBtn>
+                                    </MDBCardBody>
+                                    </MDBCard>
+                                </div>
+                            </div>
+                             )
+                         })}
+                    </div>
+                </div>
+
             </div>
         )
     }
